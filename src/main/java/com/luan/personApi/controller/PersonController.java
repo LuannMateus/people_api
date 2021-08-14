5ec625a7d8b42847b7f6162acd1b97131fc8320b
LuannMateus <luanmateuslima34@gmail.com>
2021-08-14 13:07:03 -0300

    Add findAll and save methods with DTO in Person
    Add findAll method with PersonDTO.
    Add save method with PersonDTO

--- src/main/java/com/luan/personApi/controller/PersonController.java
+++ src/main/java/com/luan/personApi/controller/PersonController.java
@@ -1,17 +1,18 @@
 package com.luan.personApi.controller;
 
 import com.luan.personApi.dto.MessageResponseDTO;
+import com.luan.personApi.dto.entityDTO.PersonDTO;
 import com.luan.personApi.entity.Person;
 import com.luan.personApi.service.PersonService;
-import org.aspectj.weaver.patterns.PerObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/person")
+@RequestMapping("/api/v1/people")
 public class PersonController {
 
     private final PersonService personService;
@@ -21,19 +22,16 @@ public class PersonController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Person>> findAll() {
-        final List<Person> allPersons = this.personService.findAll();
+    public ResponseEntity<List<PersonDTO>> findAll() {
+        List<PersonDTO> allPersonsDTO = this.personService.findAll();
 
-        return ResponseEntity.ok().body(allPersons);
+        return ResponseEntity.ok().body(allPersonsDTO);
     }
 
     @PostMapping
-    public MessageResponseDTO save(@RequestBody Person person) {
-        final Person savedPerson = this.personService.save(person);
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO save(@RequestBody @Valid PersonDTO personDTO) {
 
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+        return this.personService.save(personDTO);
     }
 }
--- src/main/java/com/luan/personApi/service/PersonService.java
+++ src/main/java/com/luan/personApi/service/PersonService.java
@@ -1,27 +1,41 @@
 package com.luan.personApi.service;
 
+import com.luan.personApi.dto.MessageResponseDTO;
+import com.luan.personApi.dto.entityDTO.PersonDTO;
 import com.luan.personApi.entity.Person;
+import com.luan.personApi.mapper.PersonMapper;
 import com.luan.personApi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private final PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public List<Person> findAll() {
-        return this.personRepository.findAll();
+    public List<PersonDTO> findAll() {
+        List<Person> allPersons = this.personRepository.findAll();
+
+        return allPersons.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 
-    public Person save(Person person) {
-        return this.personRepository.save(person);
+    public MessageResponseDTO save(PersonDTO personDTO) {
+        Person personEntity = personMapper.toModel(personDTO);
+
+        Person savedPerson = this.personRepository.save(personEntity);
+
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
     }
 
 
