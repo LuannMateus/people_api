85f63ba07b6f1216434f982a0fc972efd5032c3a
LuannMateus <luanmateuslima34@gmail.com>
2021-08-14 00:51:44 -0300

    Add findAll and save methods in PersonController

--- src/main/java/com/luan/personApi/controller/PersonController.java
+++ src/main/java/com/luan/personApi/controller/PersonController.java
@@ -1,16 +1,39 @@
 package com.luan.personApi.controller;
 
+import com.luan.personApi.dto.MessageResponseDTO;
+import com.luan.personApi.entity.Person;
+import com.luan.personApi.service.PersonService;
+import org.aspectj.weaver.patterns.PerObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("/api/v1/person")
 public class PersonController {
 
+    private final PersonService personService;
+
+    private PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
     @GetMapping
-    public ResponseEntity<String> find() {
-        return ResponseEntity.ok().body("Hello World!");
+    public ResponseEntity<List<Person>> findAll() {
+        final List<Person> allPersons = this.personService.findAll();
+
+        return ResponseEntity.ok().body(allPersons);
+    }
+
+    @PostMapping
+    public MessageResponseDTO save(@RequestBody Person person) {
+        final Person savedPerson = this.personService.save(person);
+
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
     }
 }
